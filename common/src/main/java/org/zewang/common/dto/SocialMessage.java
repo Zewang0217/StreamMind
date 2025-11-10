bd2909785bf398b71d00451b5f01b84837e89891
Gao Xinghao <182742635+Zewang0217@users.noreply.github.com>
2025-11-10 19:06:07 +0800

    feat: SocialMessage 的dto

---
+++ common/src/main/java/org/zewang/common/dto/SocialMessage.java
@@ -0,0 +1,27 @@
+package org.zewang.common.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+/**
+ * @author "Zewang"
+ * @version 1.0
+ * @description: TODO
+ * @email "Zewang0217@outlook.com"
+ * @date 2025/11/10 19:03
+ */
+
+@Builder // 作用: 创建对象
+public record SocialMessage (
+    String messageId,
+    String source,
+    String topic,
+    String userId,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    LocalDateTime timestamp,
+    String content,
+    int interactionCount
+
+) {}
