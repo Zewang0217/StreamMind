a80a1c2f54f63be2f5f093f00eeb703fdd28a87d
Gao Xinghao <182742635+Zewang0217@users.noreply.github.com>
2025-11-10 18:49:10 +0800

    feat: 2.0

---
+++ stream-analyzer/src/test/java/org/zewang/streamanalyzer/StreamAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zewang.streamanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
