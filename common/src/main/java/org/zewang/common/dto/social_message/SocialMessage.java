c9fdf8065208ef0c198ac4d0dddc4a1858164c45
Gao Xinghao <182742635+Zewang0217@users.noreply.github.com>
2025-11-10 19:48:39 +0800

    feat: S1:模拟数据采集服务完成。 能够从csv文件读取模拟消息

--- common/src/main/java/org/zewang/common/dto/social_message/SocialMessage.java
+++ common/src/main/java/org/zewang/common/dto/social_message/SocialMessage.java
@@ -8,9 +8,22 @@ import lombok.Builder;
 /**
  * @author "Zewang"
  * @version 1.0
- * @description: TODO
+ * @description: 社交媒体消息
  * @email "Zewang0217@outlook.com"
  * @date 2025/11/10 19:03
+ */
+
+/** Builder
+ * 1. 自动生成建造者模式代码：自动创建 builder() 方法和相关构建逻辑
+ * 2. 简化对象创建：避免编写冗长的构造函数或 setter 方法
+ * 3. 提高可读性：通过链式调用方式创建对象，代码更清晰
+ */
+
+/**为什么使用record？
+ * 1. 不可变性：record 默认是不可变的，线程安全
+ * 2. 简洁性：自动生成构造函数、getter 方法、equals()、hashCode() 和 toString()
+ * 3. 数据载体：专门设计用于表示不可变的数据载体类
+ * 4. 减少样板代码：无需手动编写 getter、setter 等方法
  */
 
 @Builder // 作用: 创建对象
