a3bf9160088eb092223cfb0584d9441bc55111e4
Gao Xinghao <182742635+Zewang0217@users.noreply.github.com>
2025-11-11 19:50:15 +0800

    V2.0 social monitoring (#6)
    * feat: 优化代码注释与格式
    
    * feat: 2.0
    
    * feat: SocialMessage 的dto
    
    * feat: csv模拟社交媒体消息
    
    * feat: S1:模拟数据采集服务完成。
    能够从csv文件读取模拟消息
    
    * doc: 笔记记录
    
    * feat: 增加stream-analyzer的dto
    
    * feat: Mock AI 模拟ai调用
    
    * feat: Processor基本完成，但还未调试
    
    * feat: 修改启动类配置
    
    * feat: 将创建topic移到collector模块
    
    * feat: 修改topic常量
    
    * feat: 完善批量ai分析处理器（mock）
    
    * feat: 杂项
    
    * fixed: serde冲突 #5

--- stream-analyzer/src/main/java/org/zewang/streamanalyzer/StreamAnalyzerApplication.java
+++ stream-analyzer/src/main/java/org/zewang/streamanalyzer/StreamAnalyzerApplication.java
@@ -2,8 +2,15 @@ package org.zewang.streamanalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
+@EnableKafkaStreams
 public class StreamAnalyzerApplication {
 
     public static void main(String[] args) {
