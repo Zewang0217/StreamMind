f752aaa48c3663219bee7f3114d1abb95415f8ae
Gao Xinghao <182742635+Zewang0217@users.noreply.github.com>
2025-11-18 19:05:26 +0800

    feat(collector-service): 为RSSHubFeedConfig添加@Component注解

--- collector-service/src/main/java/org/zewang/collectorservice/model/RSSHubFeedConfig.java
+++ collector-service/src/main/java/org/zewang/collectorservice/model/RSSHubFeedConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class RSSHubFeedConfig {
     private String name;
     private String route;
